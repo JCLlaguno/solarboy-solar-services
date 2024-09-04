dab2d5397d28c9a686926f6ee008d213197996f1
JCLlaguno <johnchristopherllaguno@gmail.com>
2024-09-04 18:13:57 +0800

    added services and projects section in home page

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ navBtn.addEventListener("click", (e) => {
   e.preventDefault();
   navLinks.classList.toggle("show-links");
 });
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centeredSlides: true,
+
+  // Responsive breakpoints
+  breakpoints: {
+    1200: {
+      slidesPerView: 1.8,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});

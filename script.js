5b278f15cc892fda22c5b200252651900ba448c0
JCLlaguno <johnchristopherllaguno@gmail.com>
2024-09-11 20:50:36 +0800

    added services page/initial page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use-strict";
+"use strict";
 
 const navBtn = document.querySelector(".nav-toggle-btn");
 const navLinks = document.querySelector(".nav-links");
@@ -67,38 +67,30 @@ navBtn.addEventListener("click", (e) => {
 });
 
 // LOCATION MAP
-// initialize the map on the "map" div with a given center and zoom
-const lat = 13.2548727;
-const lng = 123.6655128;
-const map = L.map("map", { scrollWheelZoom: false }).setView([lat, lng], 12);
-const markerIcon = L.icon({
-  iconUrl: "./img/marker.svg",
-  iconSize: [40, 40], // size of the icon
-  // iconAnchor: [15, 42], // half of width + height
-});
-const marker = new L.Marker([lat, lng], { icon: markerIcon });
+const loadMap = () => {
+  if (!document.getElementById("map")) return;
+  const coords = [13.2548727, 123.6655128];
+  const map = L.map("map", {
+    scrollWheelZoom: false,
+    zoom: 12,
+    center: coords,
+  });
+  const markerIcon = L.icon({
+    iconUrl: "./img/marker.svg",
+    iconSize: [40, 40],
+  });
 
-L.tileLayer("http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}", {
-  maxZoom: 20,
-  subdomains: ["mt0", "mt1", "mt2", "mt3"],
-}).addTo(map);
+  // map location marker
+  new L.Marker(coords, { icon: markerIcon }).addTo(map);
+  L.popup({ closeButton: false, offset: [1, -8] })
+    .setLatLng(coords)
+    .setContent("SolarBoy")
+    .openOn(map);
 
-marker
-  .bindTooltip("SolarBoy", {
-    permanent: true,
-    direction: "top",
-    className: "loc-label",
-    offset: [5, -25],
-  })
-  .addTo(map);
-
-document
-  .querySelector("#map")
-  .addEventListener("wheel", preventScroll, { passive: false });
-
-function preventScroll(e) {
-  e.preventDefault();
-  e.stopPropagation();
-
-  return false;
-}
+  // map bg
+  L.tileLayer("http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}", {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }).addTo(map);
+};
+loadMap();

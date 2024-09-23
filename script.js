181f06cc05402bf1606c74e436df6856568e8a55
JCLlaguno <johnchristopherllaguno@gmail.com>
2024-09-23 21:53:49 +0800

    added show-more fade animation to service on services page

--- script.js
+++ script.js
@@ -142,6 +142,7 @@ const mobileLoadMore = () => {
   const servicesList = document.querySelectorAll(".service");
   const loadMoreBtn = document.querySelector(".services-show-more");
   const showLessBtn = document.querySelector(".services-show-less");
+  const test = document.querySelector(".nav-logo");
   const servicesLength = servicesList.length;
 
   // max num of cards to be displayed
@@ -149,32 +150,45 @@ const mobileLoadMore = () => {
 
   // MAIN function to show services cards
   const showServices = (servicesList, numCards) => {
+    let delay = 0;
     servicesList.forEach((service, i) => {
       // if current index is less than or equal to numCards,
       // display it, else hide it
       // service.style.display = i <= numCards - 1 ? "grid" : "none";
       if (i <= numCards - 1) {
+        // 8
         service.style.border = "2px solid var(--fg-2)";
         service.style.padding = "1rem";
         service.style.height = "300px";
         service.style.marginBottom = "2rem";
         service.style.opacity = 1;
-        // service.style.animation = "display 2s";
+
+        // if current index is greater or equal 4 and
+        // if current index is less than or equal to numCards,
+        // increase transition delay by 200ms
+        if (i >= servicesList.length - 4) {
+          // 4
+          service.style.transitionDelay = `${delay}ms`;
+          delay += 200;
+        }
       } else {
         service.style.border = "none";
         service.style.padding = 0;
-        // service.style.paddingLeft = "1rem";
         service.style.height = 0;
         service.style.marginBottom = 0;
         service.style.opacity = 0;
+        delay = 0;
       }
     });
   };
 
   // initial page load
-  // if total cards number = 4, hide show less button
   if (!showLessBtn) return;
-  if (numCards === 4) showLessBtn.style.display = "none";
+  // if total cards number = 4, hide show less button
+  // if (numCards === 4) showLessBtn.style.display = "none";
+
+  // hide showLessBtn by default
+  showLessBtn.style.display = "none";
 
   // load 4 services
   showServices(servicesList, numCards);
@@ -260,37 +274,46 @@ const showHideDetails = () => {
 showHideDetails();
 
 const loadCustomSelect = () => {
-  // Get all custom select elements
-  let customSelects = document.querySelectorAll(".custom-select");
+  // Get all dropdowns from the document
+  const dropdowns = document.querySelectorAll(".dropdown");
 
-  // Attach click event listeners to each custom select
-  customSelects.forEach(function (select) {
-    let selectSelected = select.querySelector(".select-selected");
-    let selectItems = select.querySelector(".select-items");
-    let options = selectItems.querySelectorAll("div");
+  //   loop through all dropdown elements
+  dropdowns.forEach((dropdown) => {
+    // Get inner elements from each dropdown
+    const select = dropdown.querySelector(".select");
+    const caret = dropdown.querySelector(".caret");
+    const menu = dropdown.querySelector(".menu");
+    const options = dropdown.querySelectorAll(".menu li");
+    const selected = dropdown.querySelector(".selected");
 
-    // Toggle the dropdown visibility when the select box is clicked
-    selectSelected.addEventListener("click", function () {
-      if (selectItems.style.display === "block") {
-        selectItems.style.display = "none";
-      } else {
-        selectItems.style.display = "block";
-      }
+    // Add a click event to the select element
+    select.addEventListener("click", () => {
+      // Add the clicked select styles to the select element
+      select.classList.toggle("select-clicked");
+      //   Add the rotate styles to the caret element
+      caret.classList.toggle("caret-rotate");
+      //   Add the open styles to the menu element
+      menu.classList.toggle("menu-open");
     });
-
-    // Set the selected option and hide the dropdown when an option is clicked
-    options.forEach(function (option) {
-      option.addEventListener("click", function () {
-        selectSelected.textContent = option.textContent;
-        selectItems.style.display = "none";
+    //   Loop through all option elements
+    options.forEach((option) => {
+      // Add a click event to the option element
+      option.addEventListener("click", () => {
+        // Change selected inner text to clicked option inner text
+        selected.innerText = option.innerText;
+        //   Add clicked select styles to the select element
+        select.classList.remove("select-clicked");
+        // Add the rotate styles to the caret element
+        caret.classList.remove("caret-rotate");
+        // Add the open styles to the menu element
+        menu.classList.remove("menu-open");
+        // Remove active class from all option elements
+        options.forEach((option) => {
+          option.classList.remove("active");
+        });
+        // Add active class to clicked option element
+        option.classList.add("active");
       });
-    });
-
-    // Close the dropdown if the user clicks outside of it
-    window.addEventListener("click", function (e) {
-      if (!select.contains(e.target)) {
-        selectItems.style.display = "none";
-      }
     });
   });
 };
